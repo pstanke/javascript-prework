347fd232dd024544ec7b9f4f6a43cce9ed8cf479
Pawel Stanke <pablostanke21@gmail.com>
2022-06-29 15:57:26 +0200

    Update js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1){
+  computerMove = 'kamień';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+else if (randomNumber ==2){
+    computerMove = 'papier';
 }
 
-let playerMove = 'papier';
+else if (randomNumber ==3){
+    computerMove = 'nożyce';
+}
 
-printMessage(`Zagrałem ${playerMove}! Jeśli Twój ruch to nożyce, to wygrywasz!`);
+printMessage('Mój ruch to: ' + computerMove);
